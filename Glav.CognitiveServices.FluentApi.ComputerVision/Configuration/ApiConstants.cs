2de297e68d282f8d883d2375f91930133d644dee
Paul Glavich <glav@theglavs.com>
2017-11-03 17:16:14 +1100

    Added empty skeleton project for ComputerVision

---
+++ Glav.CognitiveServices.FluentApi.ComputerVision/Configuration/ApiConstants.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.ComputerVision.Configuration
+{
+    public static class ApiConstants
+    {
+        public const string COMPUTER_VISION_VERSION = "v2.0";
+
+    }
+}
---
+++ Glav.CognitiveServices.FluentApi.ComputerVision/Configuration/ApiServiceUriCollection.cs
@@ -0,0 +1,15 @@
+﻿using Glav.CognitiveServices.FluentApi.Core.Configuration;
+
+namespace Glav.CognitiveServices.FluentApi.ComputerVision.Configuration
+{
+    public class ApiServiceUriCollection : ApiServiceUriCollectionBase
+    {
+        public ApiServiceUriCollection()
+        {
+            //Services.Add(ApiActionType.TextAnalyticsSentiment, new SentimentServiceConfig());
+            //Services.Add(ApiActionType.TextAnalyticsKeyphrases, new KeyPhraseServiceConfig());
+            //Services.Add(ApiActionType.TextAnalyticsLanguages, new LanguageServiceConfig());
+            //Services.Add(ApiActionType.TextAnalyticsOperationStatus, new OperationStatusServiceConfig());
+        }
+    }
+}
