e955683da23f57b1b3d1720a87df39efb462ef2f
Paul Glavich <glav@theglavs.com>
2018-01-24 07:29:46 +1100

    Changed extenion methods and added tests

--- Glav.CognitiveServices.FluentApi.Emotion/EmotionImageRecognitionResponseItemExtensions.cs
+++ Glav.CognitiveServices.FluentApi.Emotion/EmotionImageRecognitionResponseItemExtensions.cs
@@ -8,10 +8,88 @@ namespace Glav.CognitiveServices.FluentApi.Emotion
 {
     public static class EmotionImageRecognitionResponseItemExtensions
     {
-        public static bool IsHappy(this ImageRecognitionAnalysisContext context, EmotionImageRecognitionResponseItem recognitionItem)
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetAngryFaces(this ImageRecognitionAnalysisContext context)
         {
-            var score = context.ScoringEngine.EvaluateScore(recognitionItem.scores.happiness);
-            return score.Name == EmotionRangeScoreLevels.DefinitelyPositive || score.Name == EmotionRangeScoreLevels.ProbablyPositive;
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Anger);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetContemptFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Contempt);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetDisgustedFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Disgust);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetFearfulFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Fear);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetHappyFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context,ImageRecognitionFaceEmotion.Happiness);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetNeutralFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Neutral);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetSadFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Sadness);
+        }
+        public static IEnumerable<EmotionImageRecognitionResponseItem> GetSurprisedFaces(this ImageRecognitionAnalysisContext context)
+        {
+            return FindResultsWithScoreDefinitelyOrPossiblyPositive(context, ImageRecognitionFaceEmotion.Surprise);
+        }
+
+        private static IEnumerable<EmotionImageRecognitionResponseItem> FindResultsWithScoreDefinitelyOrPossiblyPositive(ImageRecognitionAnalysisContext context, ImageRecognitionFaceEmotion emotionType)
+        {
+            var results = new List<EmotionImageRecognitionResponseItem>();
+            foreach (var item in context.AnalysisResult.ResponseData.faces)
+            {
+                var score = context.ScoringEngine.EvaluateScore(GetScoreToEvaluate(item,emotionType));
+                if (score.Name == EmotionRangeScoreLevels.DefinitelyPositive || score.Name == EmotionRangeScoreLevels.ProbablyPositive)
+                {
+                    results.Add(item);
+                }
+            }
+            return results;
+        }
+
+        private enum ImageRecognitionFaceEmotion
+        {
+            Anger,
+            Contempt,
+            Disgust,
+            Fear,
+            Happiness,
+            Neutral,
+            Sadness,
+            Surprise
+
+        }
+        private static double GetScoreToEvaluate(EmotionImageRecognitionResponseItem item, ImageRecognitionFaceEmotion emotionType )
+        {
+            switch (emotionType)
+            {
+                case ImageRecognitionFaceEmotion.Anger:
+                    return item.scores.anger;
+                case ImageRecognitionFaceEmotion.Contempt:
+                    return item.scores.contempt;
+                case ImageRecognitionFaceEmotion.Disgust:
+                    return item.scores.disgust;
+                case ImageRecognitionFaceEmotion.Fear:
+                    return item.scores.fear;
+                case ImageRecognitionFaceEmotion.Happiness:
+                    return item.scores.happiness;
+                case ImageRecognitionFaceEmotion.Neutral:
+                    return item.scores.neutral;
+                case ImageRecognitionFaceEmotion.Sadness:
+                    return item.scores.sadness;
+                case ImageRecognitionFaceEmotion.Surprise:
+                    return item.scores.surprise;
+            }
+
+            return 0;
         }
     }
 }
--- Tests/Glav.CognitiveServices.UnitTests/Emotion/EmotionScoreTests.cs
+++ Tests/Glav.CognitiveServices.UnitTests/Emotion/EmotionScoreTests.cs
@@ -33,20 +33,55 @@ namespace Glav.CognitiveServices.UnitTests.Emotion
             Assert.Equal(1, actual.Count());
             var actualItem = actual.First();
 
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.anger).Name, 
-                EmotionRangeScoreLevels.DefinitelyNegative);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.contempt).Name, 
-                EmotionRangeScoreLevels.DefinitelyNegative);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.disgust).Name, 
-                EmotionRangeScoreLevels.DefinitelyNegative);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.fear).Name, 
-                EmotionRangeScoreLevels.DefinitelyNegative);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.happiness).Name, 
-                EmotionRangeScoreLevels.DefinitelyPositive);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.sadness).Name,
-                EmotionRangeScoreLevels.DefinitelyNegative);
-            Assert.Equal(result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.surprise).Name, 
-                EmotionRangeScoreLevels.DefinitelyNegative);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.anger).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative, 
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.contempt).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.disgust).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.fear).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyPositive,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.happiness).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.sadness).Name);
+            Assert.Equal(EmotionRangeScoreLevels.DefinitelyNegative,
+                result.ImageRecognitionAnalysis.ScoringEngine.EvaluateScore(actualItem.scores.surprise).Name);
+        }
+
+        [Fact]
+        public async Task ShouldGetAngryFacesFromDefaultSet()
+        {
+            // Prepare our dummy response
+            var input = "[  {    \"faceRectangle\": {       \"left\": 68,       \"top\": 97,       \"width\": 64,       \"height\": 97     }" +
+                            ",     \"scores\": {       \"anger\": 0.89300731952,       \"contempt\": 5.14648448E-08,       \"disgust\": 9.180124E-06," +
+                            "       \"fear\": 0.0001912825,       \"happiness\": 0.0005571,       \"neutral\": 0.0009861537,       \"sadness\": 1.889955E-05," +
+                            "       \"surprise\": 0.008229999     }   },"+
+                            "{    \"faceRectangle\": {       \"left\": 68,       \"top\": 97,       \"width\": 64,       \"height\": 97     }" +
+                            ",     \"scores\": {       \"anger\": 0.90300731952,       \"contempt\": 5.14648448E-08,       \"disgust\": 9.180124E-06," +
+                            "       \"fear\": 0.0001912825,       \"happiness\": 0.1875571,       \"neutral\": 0.0009861537,       \"sadness\": 1.889955E-05," +
+                            "       \"surprise\": 0.008229999     }   },"+
+                            "{    \"faceRectangle\": {       \"left\": 68,       \"top\": 97,       \"width\": 64,       \"height\": 97     }" +
+                            ",     \"scores\": {       \"anger\": 0.00300731952,       \"contempt\": 5.14648448E-08,       \"disgust\": 9.180124E-06," +
+                            "       \"fear\": 0.0001912825,       \"happiness\": 0.9875571,       \"neutral\": 0.0009861537,       \"sadness\": 1.889955E-05," +
+                            "       \"surprise\": 0.008229999     }   }]";
+            var dummyResult = new MockCommsResult(input);
+            var dummyEngine = new MockCommsEngine(dummyResult);
+
+            // Run the dummy comms result through the pipeline
+            var result = await EmotionConfigurationSettings.CreateUsingConfigurationKeys("123", FluentApi.Core.LocationKeyIdentifier.EastUs2)
+                .UsingDefaultGlobalScoringEngineAndScoreLevels()
+                .UsingCustomCommunication(dummyEngine)
+                .WithEmotionAnalysisActions()
+                .AddImageRecognition("http://fakeurl.com/wont/get/called")
+                .AnalyseAllEmotionsAsync();
+
+            // Check we got what we expect
+            var actuals = result.ImageRecognitionAnalysis.GetFacesRecognised();
+            Assert.Equal(3, actuals.Count());
+
+            var angryFaces = result.ImageRecognitionAnalysis.GetAngryFaces();
+            Assert.Equal(2, angryFaces.Count());
         }
 
     }
