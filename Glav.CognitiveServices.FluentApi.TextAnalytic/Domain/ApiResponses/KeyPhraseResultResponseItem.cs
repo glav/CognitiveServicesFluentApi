cbea827048b046d45398d067975d9cfc1184b81a
Paul Glavich <glav@theglavs.com>
2018-03-02 07:36:00 +1100

    Removing some using statements

--- Glav.CognitiveServices.FluentApi.TextAnalytic/Domain/ApiResponses/KeyPhraseResultResponseItem.cs
+++ Glav.CognitiveServices.FluentApi.TextAnalytic/Domain/ApiResponses/KeyPhraseResultResponseItem.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Glav.CognitiveServices.FluentApi.TextAnalytic.Domain.ApiResponses
+﻿namespace Glav.CognitiveServices.FluentApi.TextAnalytic.Domain.ApiResponses
 {
     public sealed class KeyPhraseResultResponseItem
     {
--- Glav.CognitiveServices.FluentApi.TextAnalytic/SentenceExtensions.cs
+++ Glav.CognitiveServices.FluentApi.TextAnalytic/SentenceExtensions.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace Glav.CognitiveServices.FluentApi.TextAnalytic
 {
