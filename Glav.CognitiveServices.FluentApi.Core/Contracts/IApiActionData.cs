dec9df5cbb0d368aa79102588d1b1f56493563ef
Paul Glavich <glav@theglavs.com>
2018-05-18 08:10:31 +1000

    Well it compiles :-) Modified emotion to support multiple action data items as well

--- Glav.CognitiveServices.FluentApi.Core/Contracts/IApiActionData.cs
+++ Glav.CognitiveServices.FluentApi.Core/Contracts/IApiActionData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Glav.CognitiveServices.FluentApi.Core.Contracts
@@ -7,5 +8,17 @@ namespace Glav.CognitiveServices.FluentApi.Core.Contracts
     public interface IApiActionData
     {
         string ToUrlQueryParameters();
+
+    }
+
+    public abstract class ApiActionDataCollection<T> where T : IActionDataItem
+    {
+        protected List<T> ItemList = new List<T>();
+
+        public T[] GetAllItems()
+        {
+            return ItemList.ToArray();
+        }
+
     }
 }
--- Glav.CognitiveServices.FluentApi.Emotion/EmotionFluentApiExtensions.cs
+++ Glav.CognitiveServices.FluentApi.Emotion/EmotionFluentApiExtensions.cs
@@ -12,12 +12,14 @@ namespace Glav.CognitiveServices.FluentApi.Emotion
     {
         public static EmotionAnalysisSettings AddImageRecognition(this EmotionAnalysisSettings apiAnalysis, string url)
         {
-            apiAnalysis.ActionsToPerform.Add(ApiActionType.EmotionImageRecognition, new EmotionActionData(new Uri(url)));
+            var actionData = apiAnalysis.GetOrCreateActionDataInstance<EmotionActionData>(ApiActionType.EmotionImageRecognition);
+            actionData.Add(new Uri(url));
             return apiAnalysis;
         }
         public static EmotionAnalysisSettings AddImageRecognition(this EmotionAnalysisSettings apiAnalysis, Uri uri)
         {
-            apiAnalysis.ActionsToPerform.Add(ApiActionType.EmotionImageRecognition, new EmotionActionData(uri));
+            var actionData = apiAnalysis.GetOrCreateActionDataInstance<EmotionActionData>(ApiActionType.EmotionImageRecognition);
+            actionData.Add(uri);
             return apiAnalysis;
         }
 
