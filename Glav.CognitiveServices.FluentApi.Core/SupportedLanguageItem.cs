de9d4f29cf69159550475e710b4a25d8637c9b2f
Paul Glavich <glav@theglavs.com>
2018-06-15 18:38:40 +1000

    Moved out language classes for cleaner code

--- Glav.CognitiveServices.FluentApi.Core/SupportedLanguageItem.cs
+++ Glav.CognitiveServices.FluentApi.Core/SupportedLanguageItem.cs
@@ -21,43 +21,6 @@ namespace Glav.CognitiveServices.FluentApi.Core
         public IEnumerable<ApiActionType> ApiSupport { get { return _apiSupport.ToArray(); } }
     }
 
-    public static class LanguageListBuilder
-    {
-        private static List<SupportedLanguageItem> _languageList = new List<SupportedLanguageItem>();
-        static LanguageListBuilder()
-        {
-            BuildSupportedLanguagesList();
-        }
-        public static void BuildSupportedLanguagesList()
-        {
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Unspecified, string.Empty, new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.English, "en", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment, ApiActionType.TextAnalyticsKeyphrases, ApiActionType.ComputerVisionImageAnalysis }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Spanish, "es", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment, ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.French, "fr", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment}));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Portuguese, "pt", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment}));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.German, "de", new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Japanese, "ja", new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.SimplifiedChinese, "zh", new ApiActionType[] { ApiActionType.ComputerVisionImageAnalysis }));
-        }
-
-        public static string ToCode(this SupportedLanguageType languageType)
-        {
-            var item = _languageList.FirstOrDefault(l => l.LanguageType == languageType);
-            if (item == null)
-            {
-                return _languageList.First(l => l.LanguageType == SupportedLanguageType.English).Code;
-            }
-            return item.Code;
-        }
-
-        public static string ToUrlQueryParameter(this SupportedLanguageType languageType)
-        {
-            var code = languageType.ToCode();
-            return code == string.Empty ? string.Empty : $"{ApiConstants.LanguageUrlParameterName}={code}";
-        }
-
-    }
-
     public enum SupportedLanguageType
     {
         Unspecified,
