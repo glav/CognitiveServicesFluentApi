8f6993d482e32b063f414dbe2fbadaa2fa96afdc
Paul Glavich <glav@theglavs.com>
2018-06-19 19:55:55 +1000

    Added in full language list support for OCR

--- Glav.CognitiveServices.FluentApi.Core/LanguageListBuilder.cs
+++ Glav.CognitiveServices.FluentApi.Core/LanguageListBuilder.cs
@@ -13,14 +13,97 @@ namespace Glav.CognitiveServices.FluentApi.Core
         }
         private static void BuildSupportedLanguagesList()
         {
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Unspecified, string.Empty, new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.English, "en", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment, ApiActionType.TextAnalyticsKeyphrases, ApiActionType.ComputerVisionImageAnalysis }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Spanish, "es", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment, ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.French, "fr", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment}));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Portuguese, "pt", new ApiActionType[] { ApiActionType.TextAnalyticsSentiment}));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.German, "de", new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Japanese, "ja", new ApiActionType[] { ApiActionType.TextAnalyticsKeyphrases }));
-            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.SimplifiedChinese, "zh", new ApiActionType[] { ApiActionType.ComputerVisionImageAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Unspecified, string.Empty, 
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsKeyphrases,
+                                    ApiActionType.ComputerVisionImageAnalysis,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.English, "en",
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsSentiment,
+                                    ApiActionType.TextAnalyticsKeyphrases,
+                                    ApiActionType.ComputerVisionImageAnalysis,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Spanish, "es",
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsSentiment,
+                                    ApiActionType.TextAnalyticsKeyphrases,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.French, "fr", 
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsSentiment,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Portuguese, "pt", 
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsSentiment,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.German, "de", 
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsKeyphrases,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Japanese, "ja", 
+                                new ApiActionType[] {
+                                    ApiActionType.TextAnalyticsKeyphrases,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.SimplifiedChinese, "zh",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionImageAnalysis,
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.ChineseTraditional, "zh-Hant",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Czech, "cs",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Danish, "da",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Dutch, "nl",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Finnish, "fi",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Greek, "gr",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Hungarian, "hr",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Italian, "it",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Korean, "ko",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Norwegian, "nb",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Polish, "pl",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Russian, "ru",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Swedish, "sv",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis}));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Turkish, "tr",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Arabic, "ar",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.SerbianCyrillic, "sr-Cyrl",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.SerbianLatin, "sr-Latn",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
+            _languageList.Add(new SupportedLanguageItem(SupportedLanguageType.Slovak, "sk",
+                                new ApiActionType[] {
+                                    ApiActionType.ComputerVisionOcrAnalysis }));
         }
 
         public static SupportedLanguageItem[] GetAllSupportedLanguages()
