a0df84470a12ea53766a2132d882c399b6e61aca
Paul Glavich <glav@theglavs.com>
2018-07-26 17:21:34 +1000

    Added  unit test for parsing results

--- Tests/Glav.CognitiveServices.UnitTests/ComputerVision/RecognizeTextAnalysisParsingTests.cs
+++ Tests/Glav.CognitiveServices.UnitTests/ComputerVision/RecognizeTextAnalysisParsingTests.cs
@@ -18,18 +18,32 @@ namespace Glav.CognitiveServices.UnitTests.ComputerVision
 
         public RecognizeTextAnalysisParsingTests()
         {
-            _recognizeTextAnalysisResponse = _testHelper.GetFileDataEmbeddedInAssembly("ComputerVisionImageAnalysisResponse.json");
+            _recognizeTextAnalysisResponse = _testHelper.GetFileDataEmbeddedInAssembly("ComputerVisionRecognizeTextAnalysisResponse.json");
         }
 
         [Fact]
         public void InitialCallShouldReturnOperationStatusResult()
         {
-            var result = new RecognizeTextAnalysisResult(new MockCommsResult(HttpStatusCode.Accepted, "https://cognitiveservice/vision/v2.0/textOperations/49a36324-fc4b-4387-aa06-090cfbf0064f"));
+            int[] expeectedLineBoundingBox =  new int[] { 283, 130, 299, 50, 315, 53, 299, 133 };
+            var result = new RecognizeTextAnalysisResult(new MockCommsResult(_recognizeTextAnalysisResponse));
 
             Assert.NotNull(result);
-            Assert.NotNull(result.ApiCallResult);
-            Assert.True(result.ActionSubmittedSuccessfully);
-            Assert.NotNull(result.ApiCallResult.OperationLocationUri);
+            Assert.NotNull(result.ResponseData);
+            Assert.NotNull(result.ResponseData.recognitionResult);
+            Assert.NotEmpty(result.ResponseData.recognitionResult.lines);
+            Assert.Equal(1,result.ResponseData.recognitionResult.lines.Length);
+            Assert.Equal("test", result.ResponseData.recognitionResult.lines[0].text);
+            Assert.NotEmpty(result.ResponseData.recognitionResult.lines[0].boundingBox);
+            Assert.NotEmpty(result.ResponseData.recognitionResult.lines[0].words);
+            Assert.Equal(1, result.ResponseData.recognitionResult.lines[0].words.Length);
+
+            for (var cnt=0; cnt < expeectedLineBoundingBox.Length; cnt++)
+            {
+                Assert.Equal(expeectedLineBoundingBox[cnt],result.ResponseData.recognitionResult.lines[0].boundingBox[cnt]);
+            }
+
+
+
 
 
         }
