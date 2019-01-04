178b5e36ccda24dc7089d69c0d6487b83a11d808
Paul Glavich <glav@theglavs.com>
2019-01-04 18:48:17 +1100

    Cleanup of unused usings

--- Glav.CognitiveServices.FluentApi.ComputerVision/RecognizeTextAnalysisContextExtensions.cs
+++ Glav.CognitiveServices.FluentApi.ComputerVision/RecognizeTextAnalysisContextExtensions.cs
@@ -1,10 +1,4 @@
 ﻿using Glav.CognitiveServices.FluentApi.ComputerVision.Domain;
-using Glav.CognitiveServices.FluentApi.Core;
-using Glav.CognitiveServices.FluentApi.Core.ScoreEvaluation;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
 namespace Glav.CognitiveServices.FluentApi.ComputerVision
 {
