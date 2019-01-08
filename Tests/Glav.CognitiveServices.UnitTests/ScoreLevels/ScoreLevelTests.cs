fcbced65d3e9e19f6ebae49014c6f0f585eb8d7a
Paul Glavich <glav@theglavs.com>
2019-01-08 12:18:59 +1100

    Aded tests for custom score levels

--- Tests/Glav.CognitiveServices.UnitTests/ScoreLevels/ScoreLevelTests.cs
+++ Tests/Glav.CognitiveServices.UnitTests/ScoreLevels/ScoreLevelTests.cs
@@ -3,6 +3,8 @@ using Glav.CognitiveServices.FluentApi.Core.ScoreEvaluation;
 using System;
 using System.Linq;
 using Glav.CognitiveServices.FluentApi.Core;
+using System.Threading.Tasks;
+using Glav.CognitiveServices.FluentApi.TextAnalytic;
 
 namespace Glav.CognitiveServices.UnitTests.Emotion
 {
@@ -68,6 +70,38 @@ namespace Glav.CognitiveServices.UnitTests.Emotion
                 scoreEngine.EvaluateScore(0.4);
             });
         }
+
+        [Fact]
+        public async Task ShouldUtiliseCustomScoreLevel()
+        {
+            var langResult = "{\"documents\":[{\"id\":\"1\",\"detectedLanguages\":[{\"name\":\"English\",\"iso6391Name\":\"en\",\"score\":1.0},{\"name\":\"English\",\"iso6391Name\":\"en\",\"score\":0.45}]}],\"errors\":[]}";
+            var commsEngine = new MockCommsEngine(new MockCommsResult(langResult));
+
+            var result = await TextAnalyticConfigurationSettings.CreateUsingConfigurationKeys("test", LocationKeyIdentifier.WestUs)
+                .SetDiagnosticLoggingLevel(LoggingLevel.None)
+                .AddDebugDiagnosticLogging()
+                .UsingCustomCommunication(commsEngine)
+                .WithTextAnalyticAnalysisActions()
+                .AddLanguageAnalysis("Can be anything here")
+                .AnalyseAllAsync();
+
+            Assert.NotNull(result);
+            Assert.NotNull(result.LanguageAnalysis);
+            Assert.NotEmpty(result.LanguageAnalysis.AnalysisResults);
+
+            result.LanguageAnalysis.SetScoringEngine(new DefaultScoreEvaluationEngine(new CustomValidScoreLevelCollection()));
+
+            var results = result.LanguageAnalysis.GetResults().ToList();
+            Assert.Equal(2, results.First().detectedLanguages.Length);
+
+            var scoreResult1 = result.LanguageAnalysis.Score(results[0].detectedLanguages[0]);
+            Assert.NotNull(scoreResult1);
+            Assert.Equal(CustomValidScoreLevelCollection.HighestScore, scoreResult1.Name);
+            var scoreResult2 = result.LanguageAnalysis.Score(results[0].detectedLanguages[1]);
+            Assert.NotNull(scoreResult2);
+            Assert.Equal(CustomValidScoreLevelCollection.Level2Score, scoreResult2.Name);
+        }
+
 
 
     }
