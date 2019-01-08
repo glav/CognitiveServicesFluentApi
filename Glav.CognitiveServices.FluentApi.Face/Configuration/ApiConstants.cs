89053fce7882ef1949baf8cc4492c479420e5807
Paul Glavich <glav@theglavs.com>
2019-01-08 12:39:51 +1100

    NEW:Adding initial structure of Face API support

---
+++ Glav.CognitiveServices.FluentApi.Face/Configuration/ApiConstants.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.Face.Configuration
+{
+    public static class ApiConstants
+    {
+        public const string FACE_VERSION = "v1.0";
+    }
+}
---
+++ Glav.CognitiveServices.FluentApi.Face/Domain/ApiResponses/FaceDetectResponseRoot.cs
@@ -0,0 +1,13 @@
+﻿using Glav.CognitiveServices.FluentApi.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.Face.Domain.ApiResponses
+{
+    public class FaceDetectResponseRoot : IActionResponseRoot
+    {
+        public ApiErrorResponse error { get; set; }
+    }
+
+}
