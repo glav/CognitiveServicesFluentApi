5453d7cbc9a111695f9acf4669e4d0c5d1fb688a
Paul Glavich <glav@theglavs.com>
2019-01-22 17:40:22 +1100

    NEW:Adding context, action and result classes for LargePersonGroup support

---
+++ Glav.CognitiveServices.FluentApi.Face/Domain/LargePersonGroupActionData.cs
@@ -0,0 +1,60 @@
+﻿using Glav.CognitiveServices.FluentApi.Core.Configuration;
+using Glav.CognitiveServices.FluentApi.Core.Contracts;
+using Glav.CognitiveServices.FluentApi.Face.Domain;
+using Glav.CognitiveServices.FluentApi.Core.Communication;
+using System;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.Face.Domain
+{
+    public class LargePersonGroupActionData : ApiActionDataCollection
+    {
+        public override bool SupportsBatchingMultipleItems => false;
+
+        public void Add(string groupId,
+                string name,
+                string userData = null)
+        {
+            ItemList.Add(new LargePersonGroupActionDataItem(ItemList.Count+1, groupId,name,userData));
+        }
+
+    }
+
+    public class LargePersonGroupActionDataItem : IActionDataItem
+    {
+        public LargePersonGroupActionDataItem(long id, string groupId,
+                string name,
+                string userData = null)
+        {
+            Id = id;
+            Name = name;
+            GroupId = groupId;
+            UserData = userData;
+        }
+
+        public bool IsBinaryData => false;
+
+        public ApiActionType ApiType => ApiActionType.FaceLargePersonGroups;
+
+        public string GroupId { get; private set; }
+        public string Name { get; private set; }
+        public string UserData { get; private set; }
+
+        public long Id { get; private set; }
+
+        public byte[] ToBinary()
+        {
+            return null;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{{\"name\":\"{0}\", \"userData\":\"{1}\"}}", Name, UserData);
+        }
+
+        public string ToUrlQueryParameters()
+        {
+            return $"/{GroupId}";
+        }
+    }
+}
