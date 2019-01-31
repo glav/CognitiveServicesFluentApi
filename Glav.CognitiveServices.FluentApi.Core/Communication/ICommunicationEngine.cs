12e90ee80c4bedbbed48154d199948580db805ec
Paul Glavich <glav@theglavs.com>
2019-02-01 08:42:46 +1100

    Cleaned up communications engine with simplified structure and new APIActionDefinition support. All tests actually pass!

--- Glav.CognitiveServices.FluentApi.Core/Communication/ICommunicationEngine.cs
+++ Glav.CognitiveServices.FluentApi.Core/Communication/ICommunicationEngine.cs
@@ -7,11 +7,6 @@ namespace Glav.CognitiveServices.FluentApi.Core.Communication
     {
         Task<ICommunicationResult> CallServiceAsync(ApiActionDefinition apiActionType, string payload, string urlQueryParameters = null);
         Task<ICommunicationResult> CallServiceAsync(ApiActionDefinition apiActionType, byte[] payload, string urlQueryParameters = null);
-        //Task<ICommunicationResult> ServicePostAsync(ApiActionType apiActionType, string payload, string urlQueryParameters = null);
-        //Task<ICommunicationResult> ServicePostAsync(ApiActionType apiActionType, byte[] payload, string urlQueryParameters = null);
-        //Task<ICommunicationResult> ServicePutAsync(ApiActionType apiActionType, string payload, string urlQueryParameters = null);
-        //Task<ICommunicationResult> ServicePutAsync(ApiActionType apiActionType, byte[] payload, string urlQueryParameters = null);
-        //Task<ICommunicationResult> ServiceGetAsync(string uri, ApiActionCategory apiCategory);
         Task<ICommunicationResult> ServiceGetAsync(string uri, string category);
     }
 }
