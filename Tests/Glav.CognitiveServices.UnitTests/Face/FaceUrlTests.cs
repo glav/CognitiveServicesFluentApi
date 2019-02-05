4946c91a9ca6fef684235e005f5e96407079b76c
Paul Glavich <glav@theglavs.com>
2019-02-05 18:10:01 +1100

    Adding test and cleanup to ensure support for additional Uri fragments to allow PersonGroup functionality to work

---
+++ Tests/Glav.CognitiveServices.UnitTests/Face/FaceUrlTests.cs
@@ -0,0 +1,27 @@
+using Xunit;
+using Glav.CognitiveServices.UnitTests.Helpers;
+using Glav.CognitiveServices.FluentApi.Core;
+using Glav.CognitiveServices.UnitTests.Diagnostics;
+using System.Threading.Tasks;
+using System.Linq;
+using Glav.CognitiveServices.FluentApi.Core.ScoreEvaluation;
+using Glav.CognitiveServices.FluentApi.Face.Domain;
+using Glav.CognitiveServices.FluentApi.Face;
+
+namespace Glav.CognitiveServices.UnitTests.TextAnalytic
+{
+    public class FaceUrlTests
+    {
+        private TestDataHelper _helper = new TestDataHelper();
+
+        [Fact]
+        public void ShouldParseFaceDetectionResultSuccessfully()
+        {
+            var actionItem = new LargePersonGroupActionDataItem(1, "group_id");
+            var config = new FaceConfigurationSettings("123", LocationKeyIdentifier.AustraliaEast);
+            var actual = config.GetAbsoluteUrlForApiAction(actionItem);
+            Assert.Equal("https://australiaeast.api.cognitive.microsoft.com/face/v1.0/largepersongroups/group_id", actual);
+        }
+
+    }
+}
