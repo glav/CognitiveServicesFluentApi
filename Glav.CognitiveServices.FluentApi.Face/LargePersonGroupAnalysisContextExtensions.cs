76c0d205cfc1bb4d7b9f3b8c442ce296a1202b4a
Paul Glavich <glav@theglavs.com>
2019-02-08 07:24:41 +1100

    Added fluent extension for LargePersonGroupGet/List

---
+++ Glav.CognitiveServices.FluentApi.Face/LargePersonGroupAnalysisContextExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Glav.CognitiveServices.FluentApi.Face.Domain;
+using Glav.CognitiveServices.FluentApi.Face.Domain.ApiResponses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.Face
+{
+    public static class LargePersonGroupAnalysisContextExtensions
+    {
+        public static LargePersonGroupGetResponseItem GetResult(this LargePersonGroupGetAnalysisContext context)
+        {
+            return context.AnalysisResult.ResponseData?.LargePersonGroup;
+        }
+
+        public static IEnumerable<LargePersonGroupGetResponseItem> GetResults(this LargePersonGroupListAnalysisContext context)
+        {
+            return context.AnalysisResult.ResponseData?.LargePersonGroups?.AsEnumerable();
+        }
+
+    }
+}
