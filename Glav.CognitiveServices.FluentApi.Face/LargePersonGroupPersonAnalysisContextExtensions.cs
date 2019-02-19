010ad71d607541048fa0522b0a320f69cfc98062
Paul Glavich <glav@theglavs.com>
2019-02-20 07:24:43 +1100

    Completed impl for LargePersonGroupPerson and added initial test

---
+++ Glav.CognitiveServices.FluentApi.Face/LargePersonGroupPersonAnalysisContextExtensions.cs
@@ -0,0 +1,34 @@
+﻿using Glav.CognitiveServices.FluentApi.Face.Domain;
+using Glav.CognitiveServices.FluentApi.Face.Domain.ApiResponses;
+using Glav.CognitiveServices.FluentApi.Face.Domain.LargePersonGroup;
+using Glav.CognitiveServices.FluentApi.Face.Domain.LargePersonGroupPerson;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Glav.CognitiveServices.FluentApi.Face
+{
+    public static class LargePersonGroupPersonAnalysisContextExtensions
+    {
+        public static LargePersonGroupPersonGetResponseItem GetResult(this LargePersonGroupPersonGetAnalysisContext context)
+        {
+            return context.AnalysisResult.ResponseData?.LargePersonGroupPerson;
+        }
+
+        public static IEnumerable<LargePersonGroupPersonGetResponseItem> GetResults(this LargePersonGroupPersonListAnalysisContext context)
+        {
+            return context.AnalysisResults.SelectMany(r => r.ResponseData?.LargePersonGroupPersons)?.AsEnumerable();
+        }
+
+        public static bool IsSuccessfull(this LargePersonGroupPersonCreateAnalysisContext context)
+        {
+            return context.AnalysisResults.All(r => r.ActionSubmittedSuccessfully && r.ApiCallResult.Successfull);
+        }
+        public static bool IsSuccessfull(this LargePersonGroupPersonDeleteAnalysisContext context)
+        {
+            return context.AnalysisResults.All(r => r.ActionSubmittedSuccessfully && r.ApiCallResult.Successfull);
+        }
+
+    }
+}
---
+++ Tests/Glav.CognitiveServices.UnitTests/Face/LargePersonGroupPersonTests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using Glav.CognitiveServices.UnitTests.Helpers;
+using Glav.CognitiveServices.FluentApi.Core;
+using System.Threading.Tasks;
+using Glav.CognitiveServices.FluentApi.Face;
+
+namespace Glav.CognitiveServices.UnitTests.TextAnalytic
+{
+    public class LargePersonGroupPersonTests
+    {
+        private TestDataHelper _helper = new TestDataHelper();
+
+        [Fact]
+        public async Task ShouldParseFaceDetectionResultSuccessfully()
+        {
+            var commsEngine = new MockCommsEngine(new MockCommsResult(null, System.Net.HttpStatusCode.OK));
+            var result = await FaceConfigurationSettings.CreateUsingConfigurationKeys("123", LocationKeyIdentifier.AustraliaEast)
+                .AddConsoleDiagnosticLogging()
+                .UsingCustomCommunication(commsEngine)
+                .WithFaceAnalysisActions()
+                .DeleteLargePersonGroupPerson("123","456")
+                .AnalyseAllAsync();
+
+            Assert.NotNull(result.LargePersonGroupPersonDeleteAnalysis);
+            Assert.NotNull(result.LargePersonGroupPersonDeleteAnalysis.AnalysisResult);
+            Assert.NotNull(result.LargePersonGroupPersonDeleteAnalysis.AnalysisResult.ApiCallResult);
+            Assert.True(result.LargePersonGroupPersonDeleteAnalysis.AnalysisResult.ActionSubmittedSuccessfully);
+            Assert.True(result.LargePersonGroupPersonDeleteAnalysis.IsSuccessfull());
+        }
+
+    }
+
+}
