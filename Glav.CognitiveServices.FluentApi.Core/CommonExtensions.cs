d4441e6ff2c2df4ce163d29e5f82b658f3ad550d
Paul Glavich <glav@theglavs.com>
2019-07-25 17:31:15 +1000

    Minor addition of method info

--- Glav.CognitiveServices.FluentApi.Core/CommonExtensions.cs
+++ Glav.CognitiveServices.FluentApi.Core/CommonExtensions.cs
@@ -8,6 +8,12 @@ namespace Glav.CognitiveServices.FluentApi.Core
     /// </summary>
     public static class CommonExtensions
     {
+        /// <summary>
+        /// Evaluate the score or confidence level value using the scoring engine of the API being used.
+        /// </summary>
+        /// <param name="context">API Context</param>
+        /// <param name="scoreValue">The value to parse</param>
+        /// <returns></returns>
         public static ScoreLevelBoundsDefinition Score<T>(this BaseApiAnalysisContext<T> context, double scoreValue) where T : IApiCallResult
         {
             return context.ScoringEngine.EvaluateScore(scoreValue);
