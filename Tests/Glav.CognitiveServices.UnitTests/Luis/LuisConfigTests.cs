945a4ec8b9dcc2d1980ccc3e7f093a9623157ba0
Paul Glavich <glav@theglavs.com>
2020-06-11 11:46:45 +1000

    Removed commented code

--- Tests/Glav.CognitiveServices.UnitTests/Luis/LuisConfigTests.cs
+++ Tests/Glav.CognitiveServices.UnitTests/Luis/LuisConfigTests.cs
@@ -9,25 +9,6 @@ namespace Glav.CognitiveServices.UnitTests.uis
 {
     public class LuisConfigTests
     {
-        // [Fact]
-        // public void ShouldProvideValidUrlsForLocationSpecificConfig()
-        // {
-        //     var expectedList = new List<Tuple<FluentApi.Core.LocationKeyIdentifier, string>>(new[]
-        //     {
-        //         new Tuple<FluentApi.Core.LocationKeyIdentifier, string>(FluentApi.Core.LocationKeyIdentifier.WestUs, "https://westus.api.cognitive.microsoft.com/"),
-        //         new Tuple<FluentApi.Core.LocationKeyIdentifier, string>(FluentApi.Core.LocationKeyIdentifier.AustraliaEast, "https://australiaeast.api.cognitive.microsoft.com/"),
-        //         new Tuple<FluentApi.Core.LocationKeyIdentifier, string>(FluentApi.Core.LocationKeyIdentifier.EastUs2, "https://eastus2.api.cognitive.microsoft.com/"),
-        //         new Tuple<FluentApi.Core.LocationKeyIdentifier, string>(FluentApi.Core.LocationKeyIdentifier.AustraliaSouthEast, "https://australiasoutheast.api.cognitive.microsoft.com/"),
-        //         new Tuple<FluentApi.Core.LocationKeyIdentifier, string>(FluentApi.Core.LocationKeyIdentifier.Global, "https://api.cognitive.microsoft.com/"),
-        //     });
-
-        //     expectedList.ForEach(i =>
-        //     {
-        //         var config = uisConfigurationSettings.CreateUsingConfigurationKeys("12345", i.Item1);
-        //         var url = config.BaseUrl;
-        //         Assert.Equal(i.Item2, url);
-        //     });
-        // }
 
         [Fact]
         public void ShouldHaveDefinedUrisForuisActions()
