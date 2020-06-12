df0868e82670b45436cd84ad5233c51ff2ca818f
Paul Glavich <glav@theglavs.com>
2020-06-12 16:31:51 +1000

    Begin adding context and response analysis classes, plus a contribution instructions (WIP) as I go along

---
+++ Glav.CognitiveServices.FluentApi.Luis/Domain/LuisAppAnalysisResult.cs
@@ -0,0 +1,30 @@
+﻿using Glav.CognitiveServices.FluentApi.Core.Communication;
+using Glav.CognitiveServices.FluentApi.Core.Parsing;
+using Glav.CognitiveServices.FluentApi.Luis.Domain.ApiResponses;
+using System;
+
+namespace Glav.CognitiveServices.FluentApi.Luis.Domain
+{
+    public class LuisAppAnalysisResult : BaseApiResponseReturnsData<LuisAppResponseRoot, LuisAppResponseRoot, BaseApiErrorResponse>
+    {
+        public LuisAppAnalysisResult(ICommunicationResult apiCallResult) : base(apiCallResult)
+        {
+            ParseResponseData();
+            if (!ActionSubmittedSuccessfully)
+            {
+                ResponseData = new LuisAppResponseRoot { error = ParsingStrategy.ResponseError };
+                return;
+            }
+            ResponseData = ParsingStrategy.ResponseData;
+
+            PerformCustomResponseParsing();
+
+        }
+
+        private void PerformCustomResponseParsing()
+        {
+            //TODO: Do the specialised parsing of response required for LUis response.
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Wiki/Contribution.md
@@ -0,0 +1,15 @@
+= [Home](/README.md)/Contribution
+
+### This section is VERY incomplete - WIP
+
+If you want to add a new integration with a new cognitive service
+
+* Create a new project
+* Glav.CognitiveServices.FluentApi.{cognitive service}
+* Add reference to Core project
+* Start with the 'Configuration' directory/elements (ApiConstants, ApiServiceUriCollection, *ServiceCOnfig classes)
+* Add in 'Domain' directory starting with ApiOperations
+* Add in ApiResponses to Domain namespace
+* Do AnalysisResult class and AnalysisContext in Domain namespace
+* 
+
