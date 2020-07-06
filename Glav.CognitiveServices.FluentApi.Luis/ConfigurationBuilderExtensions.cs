96cb24b989a9052cb73dd0970f7df36b1fe9cc19
Paul Glavich <glav@theglavs.com>
2020-07-06 14:58:18 +1000

    Added Luis ConfigurationBuilderExtensions

---
+++ Glav.CognitiveServices.FluentApi.Luis/ConfigurationBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Glav.CognitiveServices.FluentApi.Core;
+using Glav.CognitiveServices.FluentApi.Luis.Configuration;
+
+namespace Glav.CognitiveServices.FluentApi.Luis
+{
+    public static class ConfigurationBuilderExtensions
+    {
+        public static LuisAnalysisSettings WithLuisAnalysisActions(this CoreAnalysisSettings analysisSettings)
+        {
+            return new LuisAnalysisSettings(analysisSettings);
+        }
+
+
+    }
+}
