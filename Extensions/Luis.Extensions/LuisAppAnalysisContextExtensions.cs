ecf42b65b86328f70fbed245f6d5f6afa0bb6f1c
Paul Glavich <glav@theglavs.com>
2020-07-14 17:10:16 +1000

    NEW:Added GetTopIntents extension for LUIS results and a test to verify

---
+++ Extensions/Luis.Extensions/LuisAppAnalysisContextExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Glav.CognitiveServices.FluentApi.Luis.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Glav.CognitiveServices.FluentApi.Luis
+{
+    public static class LuisAppAnalysisContextExtensions
+    {
+        public static IEnumerable<string> GetTopIntents(this LuisAppAnalysisContext analysisContext)
+        {
+            return analysisContext.AnalysisResults.Select(i => i.ResponseData?.prediction?.topIntent);
+        }
+    }
+}
---
+++ Tests/Glav.CognitiveServices.UnitTests/Luis/LuisExtensionTests.cs
@@ -0,0 +1,39 @@
+﻿using Glav.CognitiveServices.FluentApi.Core;
+using Glav.CognitiveServices.FluentApi.Luis;
+using Glav.CognitiveServices.FluentApi.Luis.Domain;
+using Glav.CognitiveServices.UnitTests.Helpers;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Glav.CognitiveServices.UnitTests.Luis
+{
+
+    public class LuisExtensionTests
+    {
+        private TestDataHelper _helper = new TestDataHelper();
+        private string _inputData;
+
+        public LuisExtensionTests()
+        {
+            _inputData = _helper.GetFileDataEmbeddedInAssembly("LuisAppResponse.json");
+        }
+        [Fact]
+        public async Task ShouldGetTopIntents()
+        {
+            
+            var result = await LuisConfigurationSettings.CreateUsingConfigurationKeys("APPID", "sub-key",LocationKeyIdentifier.AustraliaEast)
+                .SetDiagnosticLoggingLevel(LoggingLevel.Everything)
+                .AddConsoleAndTraceLogging()
+                .UsingCustomCommunication(new MockCommsEngine(new MockCommsResult(_inputData)))
+                .WithLuisAnalysisActions()
+                .AddLuisAnalysis("whatevs")
+                .AnalyseAllAsync();
+
+            var topIntents = result.LuisAppAnalysis.GetTopIntents();
+            Assert.NotEmpty(topIntents);
+            Assert.Equal(1, topIntents.Count());
+            Assert.Equal("Find Cloud based resources", topIntents.First());
+        }
+    }
+}
